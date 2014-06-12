4d0aca60a84ad8f8375820edfd7f7577280bb91a
namit <n4khulla@uwaterloo.ca>
2014-06-11 21:58:20 -0400

    didn't do shit, added a borken #include tag

--- utopian.cpp
+++ utopian.cpp
@@ -1,26 +1,25 @@
-nclude <iostream>
+#include <iostream>
 using namespace std;
 
 int height(int n) {
-        int height1 = n;
-            if(height1<=2){
-                        return height1+1;
-                            }else {
-                                        if(height1 % 2 == 0){
-                                                        height1 = height(height1-1)+1;
-                                                                }else{
-                                                                                height1 = height(height1-1)*2;
-                                                                                        }
-                                            }
-                return height1;
+    int height1 = n;
+    if(height1<=2){
+        return height1+1;
+    }else {
+        if(height1 % 2 == 0){
+            height1 = height(height1-1)+1;                      }else{
+            height1 = height(height1-1)*2;                      }
+    }
+return height1;
 }
+
 int main() {
-        int T;
-            cin >> T;
-                while (T--) {
-                            int n;
-                                    cin >> n;
-                                            cout << height(n) << endl;
-                                                }
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << height(n) << endl;
+    }
 }
 
